c9c5b4e13a3a0dfbc9fff08c605673dc5d604c18
Florent Belotti <florent.l.d.belotti@gmail.com>
2024-04-07 18:35:03 +0200

    base color white

--- FdF_parsing/FdF_node_management.c
+++ FdF_parsing/FdF_node_management.c
@@ -6,7 +6,7 @@
 /*   By: fbelotti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 17:58:25 by fbelotti          #+#    #+#             */
-/*   Updated: 2024/04/07 18:28:19 by fbelotti         ###   ########.fr       */
+/*   Updated: 2024/04/07 18:33:58 by fbelotti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	assign_sub_token_data(char **sub_token, t_map *new_node, t_data *data)
 void	assign_token_data(char *token, t_map *new_node, t_data *data)
 {
 	new_node->pos_z = ft_atoi(token);
-	new_node->color_code = 0xFF00FF;
+	new_node->color_code = 0xFFFFFF;
 	new_node->pos_x = data->standard_x;
 	new_node->pos_y = data->standard_y;
 	new_node->save_x = data->standard_x;
